95c997a194173049060081cfc2212dd98a92952b
musicaldoofus <burns.trainingdesign@gmail.com>
2020-01-30 13:33:00 -0800

    Updates

---
+++ precache-manifest.ec7211da221959e058d80de2199fa6a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9aac35703992735e507e3239739a92",
+    "url": "/roguelike-scratch/index.html"
+  },
+  {
+    "revision": "52b1af7d7d5b7b4c6321",
+    "url": "/roguelike-scratch/static/css/main.03f36bc2.chunk.css"
+  },
+  {
+    "revision": "de803d153e26d5358a97",
+    "url": "/roguelike-scratch/static/js/2.8b60723f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/roguelike-scratch/static/js/2.8b60723f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52b1af7d7d5b7b4c6321",
+    "url": "/roguelike-scratch/static/js/main.5bc41d11.chunk.js"
+  },
+  {
+    "revision": "b3a03fe44e1161df474d",
+    "url": "/roguelike-scratch/static/js/runtime-main.ae9dd106.js"
+  },
+  {
+    "revision": "4ea29ecda21653f9238eb2da8633efe4",
+    "url": "/roguelike-scratch/static/media/Arvin.4ea29ecd.ttf"
+  },
+  {
+    "revision": "a75d3f2a2749692f43e4f5523c498e0a",
+    "url": "/roguelike-scratch/static/media/rl_avatar.a75d3f2a.svg"
+  }
+]);
